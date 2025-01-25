2cdca69a54690373f8287f7bb5fa9b6390027d70
1nsig <110944828+1nsig@users.noreply.github.com>
2025-01-25 20:56:42 +1100

    TMP Files

---
+++ best-bubble-game/Assets/SceneTransitioner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
